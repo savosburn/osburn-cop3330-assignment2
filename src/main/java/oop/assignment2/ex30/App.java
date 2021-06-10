8593ad160fd1f3ea46542b2babfcbb16a24d0d66
savosburn <savosburn18@tampabay.rr.com>
2021-06-10 10:42:25 -0400

    copyright and App object created in all

--- src/main/java/oop/assignment2/ex30/App.java
+++ src/main/java/oop/assignment2/ex30/App.java
@@ -1,8 +1,13 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Savannah Osburn
+ */
+
 package oop.assignment2.ex30;
 
 public class App {
     public static void main(String[] args) {
-        
+        App myApp = new App();
     }
 }
 
--- src/main/java/oop/assignment2/ex32/App.java
+++ src/main/java/oop/assignment2/ex32/App.java
@@ -1,7 +1,12 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Savannah Osburn
+ */
+
 package oop.assignment2.ex32;
 
 public class App {
     public static void main(String[] args) {
-
+        App myApp = new App();
     }
 }

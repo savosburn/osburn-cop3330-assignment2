019c441d8f7a4cb18dd714ade61a64925672a93b
savosburn <savosburn18@tampabay.rr.com>
2021-06-10 10:45:52 -0400

    started including assignment comments

--- src/main/java/oop/assignment2/ex24/App.java
+++ src/main/java/oop/assignment2/ex24/App.java
@@ -5,6 +5,24 @@
 
 package oop.assignment2.ex24;
 
+/*
+Using functions to abstract the logic away from the rest of your code makes it easier to read and easier to maintain.
+
+Create a program that compares two strings and determines if the two strings are anagrams.
+The program should prompt for both input strings and display the output as shown in the example that follows.
+
+Example Output
+Enter two strings and I'll tell you if they are anagrams:
+Enter the first string: note
+Enter the second string: tone
+"note" and "tone" are anagrams.
+
+Constraints
+Implement the program using a function called isAnagram, which takes in two words as its arguments
+and returns true or false. Invoke this function from your main program.
+Check that both words are the same length.
+ */
+
 public class App {
     public static void main(String[] args) {
         App myApp = new App();
--- src/main/java/oop/assignment2/ex27/App.java
+++ src/main/java/oop/assignment2/ex27/App.java
@@ -5,6 +5,47 @@
 
 package oop.assignment2.ex27;
 
+/*
+Large functions aren’t very usable or maintainable. It makes a lot of sense to break down the logic of a
+ program into smaller functions that do one thing each. The program can then call these functions in sequence to
+  perform the work.
+
+Write a program that prompts for a first name, last name, employee ID, and ZIP code. Ensure that the input
+is valid according to these rules:
+
+The first name must be filled in.
+The last name must be filled in.
+The first and last names must be at least two characters long.
+An employee ID is in the format AA-1234. So, two letters, a hyphen, and four numbers.
+The ZIP code must be a number.
+Display appropriate error messages on incorrect data.
+
+Example Output
+Enter the first name: J
+Enter the last name:
+Enter the ZIP code: ABCDE
+Enter the employee ID: A12-1234
+
+The first name must be at least 2 characters long.
+The last name must be at least 2 characters long.
+The last name must be filled in.
+The employee ID must be in the format of AA-1234.
+The zipcode must be a 5 digit number.
+
+Or
+
+Enter the first name: John
+Enter the last name: Johnson
+Enter the ZIP code: 55555
+Enter the employee ID: TK-4321
+There were no errors found.
+
+Constraints
+Create a function for each type of validation you need to write.
+Then create a validateInput function that takes in all of the input data and invokes the specific validation functions.
+Use a single output statement to display the outputs
+ */
+
 public class App {
     public static void main(String[] args) {
         App myApp = new App();

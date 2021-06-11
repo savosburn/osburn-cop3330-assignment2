bbd939b73ddbbb3e3c970e07e87031a90bffb803
savosburn <savosburn18@tampabay.rr.com>
2021-06-11 01:15:39 -0400

    test eventually :/

--- src/main/java/oop/assignment2/ex36/App.java
+++ src/main/java/oop/assignment2/ex36/App.java
@@ -5,8 +5,93 @@
 
 package oop.assignment2.ex36;
 
+/*
+Statistics is important in our field. When measuring response times or rendering times,
+it’s helpful to collect data so you can easily spot abnormalities.
+For example, the standard deviation helps you determine which values are outliers and which values are
+ within normal ranges.
+
+Write a program that prompts for response times from a website in milliseconds.
+It should keep asking for values until the user enters “done.”
+
+The program should print the average time (mean), the minimum time, the maximum time, and the population standard deviation.
+
+Example Output
+Enter a number: 100
+Enter a number: 200
+Enter a number: 1000
+Enter a number: 300
+Enter a number: done
+
+Numbers: 100, 200, 1000, 300
+The average is 400.0
+The minimum is 100
+The maximum is 1000
+The standard deviation is 353.55
+
+Constraints
+Use functions called average, max, min, and std, which take in a list of numbers and return the results.
+Use loops and arrays to perform the input and mathematical operations.
+Be sure to exclude the "done" entry from the inputs.
+Be sure to properly ignore any invalid inputs.
+Keep the input separate from the processing and the output.
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+    private static final Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
         App myApp = new App();
+        StatisticsData sd = new StatisticsData();
+        ArrayList<Double> responseTimes = new ArrayList<Double>();
+
+         responseTimes = myApp.readInput(responseTimes);
+
+        myApp.printResponseTimes(responseTimes);
+
+        String output = sd.generateOutput(responseTimes);
+        myApp.output(output);
+    }
+
+    public ArrayList<Double> readInput(ArrayList<Double> items) {
+        String input = null;
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            try {
+                input = (in.next().toLowerCase());
+                if (input.equals("done")) {
+                    break;
+                }
+                items.add(Double.parseDouble(input));
+
+            } catch (NumberFormatException e) {
+                System.out.println("Sorry. That's not a valid input. ");
+            }
+        }
+
+        return items;
+    }
+
+    public void printResponseTimes(ArrayList<Double> times){
+        System.out.print("\nNumbers: ");
+
+        for (int i = 0; i < times.size(); i++) {
+
+            if (i == times.size() - 1){
+                System.out.printf("%.0f\n", times.get(i));
+                break;
+            }
+
+            System.out.printf("%.0f, ", times.get(i));
+
+        }
+    }
+
+    public void output(String output) {
+        System.out.print(output);
     }
 }
---
+++ src/main/java/oop/assignment2/ex36/StatisticsData.java
@@ -0,0 +1,65 @@
+package oop.assignment2.ex36;
+
+import java.util.ArrayList;
+
+public class StatisticsData {
+
+    public String generateOutput(ArrayList<Double> responseTimes) {
+        StatisticsData sd = new StatisticsData();
+
+        double average = sd.average(responseTimes);
+        double max = sd.max(responseTimes);
+        double min = sd.min(responseTimes);
+        double std = sd.std(responseTimes, average);
+
+        return String.format("The average is %.01f\n", average) +
+                String.format("The minimum is %.0f\n", min) +
+                String.format("The maximum is %.0f\n", max) +
+                String.format("The standard deviation is %.02f\n", std);
+
+
+    }
+
+    public double average(ArrayList <Double> responseTimes) {
+        int total = 0;
+
+        for (Double responseTime : responseTimes) {
+            total += responseTime;
+        }
+
+        return (double) total / responseTimes.size();
+
+    }
+
+    public double max(ArrayList<Double> responseTimes) {
+        double max = Double.MIN_VALUE;
+
+        for (Double responseTime : responseTimes) {
+            if (responseTime > max) {
+                max = responseTime;
+            }
+        }
+        return max;
+    }
+
+    public double min(ArrayList<Double> responseTimes) {
+        double min = Double.MAX_VALUE;
+
+        for (Double responseTime : responseTimes) {
+            if (responseTime < min) {
+                min = responseTime;
+            }
+        }
+        return min;
+    }
+
+    public double std(ArrayList<Double> responseTimes, double average) {
+        double num = 0;
+        for (Double responseTime : responseTimes) {
+            num += Math.pow((responseTime - average), 2);
+        }
+
+        return Math.sqrt(num / responseTimes.size());
+    }
+
+}

eaec431307d73c54bb6a36c40e34835094f8fbca
savosburn <savosburn18@tampabay.rr.com>
2021-06-11 00:16:00 -0400

    figure out how to test

--- src/main/java/oop/assignment2/ex35/App.java
+++ src/main/java/oop/assignment2/ex35/App.java
@@ -5,8 +5,49 @@
 
 package oop.assignment2.ex35;
 
+/*
+Arrays don’t have to be hard-coded. You can take user input and store it in an array and then work with it.
+
+Create a program that picks a winner for a contest or prize drawing.
+Prompt for names of contestants until the user leaves the entry blank. Then randomly select a winner.
+
+Example Output
+Enter a name: Homer
+Enter a name: Bart
+Enter a name: Maggie
+Enter a name: Lisa
+Enter a name: Moe
+Enter a name:
+The winner is... Maggie.
+
+Constraints
+Use a loop to capture user input into an array.
+Use a random number generator to pluck a value from the array.
+Don’t include a blank entry in the array.
+Some languages require that you define the length of the array ahead of time.
+You may need to find another data structure, like an ArrayList.
+ */
+
+import java.util.ArrayList;
+
 public class App {
+
+
     public static void main(String[] args) {
         App myApp = new App();
+        Contest contest = new Contest();
+        ArrayList<String> contestants = new ArrayList<String>();
+
+        ArrayList<String> addedContestants = contest.addContestants(contestants);
+
+        int winner = contest.chooseWinner(addedContestants.size());
+        String output = contest.winner(addedContestants, winner);
+        myApp.print(output);
     }
+
+    public void print(String output) {
+        System.out.println(output);
+    }
+
+
 }
---
+++ src/main/java/oop/assignment2/ex35/Contest.java
@@ -0,0 +1,37 @@
+package oop.assignment2.ex35;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Contest {
+    private static final Scanner in = new Scanner(System.in);
+    Random randomNumbers = new Random();
+
+    public ArrayList<String> addContestants(ArrayList<String> contestants) {
+
+        String input = null;
+        while (true) {
+            System.out.print("Enter a name: ");
+            try {
+                input = in.nextLine();
+                if (input.isEmpty()) {
+                    break;
+                }
+                contestants.add(input);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Sorry. That's not a valid input. ");
+            }
+        }
+        return contestants;
+    }
+
+    public int chooseWinner(int numContestants) {
+        return randomNumbers.nextInt(numContestants);
+    }
+
+    public String winner(ArrayList<String> contestants, int winner) {
+        return String.format("The winner is... %s", contestants.get(winner));
+    }
+}

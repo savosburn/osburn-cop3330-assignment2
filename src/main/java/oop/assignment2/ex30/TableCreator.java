f30b7b1c31fa1619ff3148a35365fc7eb90232bd
savosburn <savosburn18@tampabay.rr.com>
2021-06-10 20:47:03 -0400

    formatting

--- src/main/java/oop/assignment2/ex30/TableCreator.java
+++ src/main/java/oop/assignment2/ex30/TableCreator.java
@@ -6,23 +6,24 @@ public class TableCreator {
     public String createTable() {
 
         int i, j;
-        String outputString = null;
+        StringBuilder outputString = null;
 
         for (i = 1; i <= MAX; i++){
             for (j = 1; j <= MAX; j++) {
                 String num = Integer.toString(i * j);
 
                 if (outputString == null) {
-                    outputString = String.format("%5s", num);
+                    outputString = new StringBuilder(String.format("%5s", num));
                 }
                 else {
-                    outputString += String.format("%5s", num);
+                    outputString.append(String.format("%5s", num));
                 }
             }
 
-            outputString += "\n";
+            outputString.append("\n");
         }
 
-        return outputString;
+        assert outputString != null;
+        return outputString.toString();
     }
 }

8e60dbb39370b543535856a983f328470ba68446
savosburn <savosburn18@tampabay.rr.com>
2021-06-13 17:57:45 -0400

    dinner break

--- src/main/java/oop/assignment2/ex40/RecordSearcher.java
+++ src/main/java/oop/assignment2/ex40/RecordSearcher.java
@@ -16,17 +16,8 @@ public class RecordSearcher {
         RecordSearcher search = new RecordSearcher();
 
         String userSearch = search.userSearch();
-        ArrayList<HashMap<String, String>> containsString = new ArrayList<>();
 
-       // containsString = search.searchFirstNames(userSearch, employeeRecords);
-
-
-        containsString.addAll(search.searchFirstNames(userSearch, employeeRecords));
-
-       // System.out.print(containsString.size());
-
-
-        return containsString;
+        return new ArrayList<>(search.searchFirstNames(userSearch, employeeRecords));
     }
 
     public String userSearch() {
@@ -39,24 +30,11 @@ public class RecordSearcher {
         ArrayList<HashMap<String, String>> temp = new ArrayList<>();
         String[] tempString = {"firstNames", "lastNames"};
 
-        //System.out.print(employeeRecords.get(2).get(tempString[0]));
-        //System.out.print(employeeRecords.get(1).get(tempString[0]).contains(userSearch));
-
-
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 6; j++) {
 
                 if (employeeRecords.get(j).get(tempString[i]).contains(userSearch)) {
-                    //System.out.print(temp.get(j).get("lastNames"));
-
-                    HashMap<String, String> tempList = new HashMap<>();
-                    tempList.put("firstNames", employeeRecords.get(j).get(tempString[i]));
-                    tempList.put("lastNames", employeeRecords.get(j).get(tempString[i]));
-                    tempList.put("positions", employeeRecords.get(j).get("positions"));
-                    tempList.put("sepDates", employeeRecords.get(j).get("sepDates"));
-
                     temp.add(employeeRecords.get(j));
-
                 }
             }
         }

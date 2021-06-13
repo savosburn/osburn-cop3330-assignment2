0fe8536f147fba2fd737a850d141080a64598bd3
savosburn <savosburn18@tampabay.rr.com>
2021-06-13 19:45:25 -0400

    completed

--- src/main/java/oop/assignment2/ex32/App.java
+++ src/main/java/oop/assignment2/ex32/App.java
@@ -7,51 +7,6 @@
 
 package oop.assignment2.ex32;
 
-/*
-Write a Guess the Number game that has three levels of difficulty.
-The first level of difficulty would be a number between 1 and 10.
-The second difficulty set would be between 1 and 100.
-The third difficulty set would be between 1 and 1000.
-
-Prompt for the difficulty level, and then start the game.
-The computer picks a random number in that range and prompts the player to guess that number.
-Each time the player guesses, the computer should give the player a hint as to whether the number is too high or too low.
-The computer should also keep track of the number of guesses.
-Once the player guesses the correct number, the computer should present the number of guesses and ask if the player
-would like to play again.
-
-Example Output
-Let's play Guess the Number!
-
-Enter the difficulty level (1, 2, or 3): 1
-I have my number. What's your guess? 5
-Too low. Guess again: 7
-Too low. Guess again: 9
-You got it in 3 guesses!
-
-Do you wish to play again (Y/N)? y
-
-Enter the difficulty level (1, 2, or 3): 3
-I have my number. What's your guess? 500
-Too low. Guess again: 750
-Too high. Guess again: 600
-Too low. Guess again: 700
-Too low. Guess again: 725
-Too high. Guess again: 715
-Too high. Guess again: 710
-Too high. Guess again: 705
-Too high. Guess again: 701
-Too low. Guess again: 702
-You got it in 10 guesses!
-
-Do you wish to play again (Y/N)? n
-Constraints
-Don’t allow any non-numeric data entry.
-During the game, count non-numeric entries as wrong guesses.
- */
-
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
         App myApp = new App();
--- src/test/java/oop/assignment2/ex32/GuessingGameTest.java
+++ src/test/java/oop/assignment2/ex32/GuessingGameTest.java
@@ -1,3 +1,10 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Savannah Osburn
+ */
+
+// FIGURE OUT HOW TO DO TEST CASES
+
 package oop.assignment2.ex32;
 
 import org.junit.jupiter.api.Test;
@@ -8,52 +15,14 @@ import java.io.InputStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GuessingGameTest {
-
     @Test
-    void test() {
-        String expected = """
-                I have my number. What's your guess? 500
-                Too low. Guess again: 750
-                Too high. Guess again: 600
-                Too low. Guess again: 700
-                Too low. Guess again: 725
-                Too high. Guess again: 715
-                Too high. Guess again: 710
-                Too high. Guess again: 705
-                Too high. Guess again: 701
-                Too low. Guess again: 702
-                You got it in 10 guesses!""";
-    }
+    void testOutput() {
 
-    @Test
-    void game() {
-    }
-
-    @Test
-    void difficulty() {
         GuessingGame game = new GuessingGame();
 
-        int expected = 3;
-        String input = "3";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        String expected = "You got it in 10 guesses!\n\n";
+        String actual =game.output(10);
 
-        assertEquals("3", game.difficulty());
-    }
-
-    @Test
-    void randomNum() {
-    }
-
-    @Test
-    void userGuesses() {
-    }
-
-    @Test
-    void output() {
-    }
-
-    @Test
-    void playAgain() {
+        assertEquals(expected, actual);
     }
 }

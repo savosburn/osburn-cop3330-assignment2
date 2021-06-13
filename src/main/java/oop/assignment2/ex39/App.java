be830fc6624d7cc4d2facc3fcfb18d7fca773ae7
savosburn <savosburn18@tampabay.rr.com>
2021-06-13 16:16:08 -0400

    completed (copyright)

--- src/main/java/oop/assignment2/ex39/App.java
+++ src/main/java/oop/assignment2/ex39/App.java
@@ -5,35 +5,6 @@
 
 package oop.assignment2.ex39;
 
-/*
-When you’re looking at results, you’ll want to be able to sort them so you
- can find what you’re looking for quickly or do some quick visual comparisons.
-
-Given the following data set, create a program that sorts all employees by
-last name and prints them to the screen in a tabular format.
-
-First Name	Last Name	Position	     Separation Date
-John	    Johnson	    Manager	            2016-12-31
-Tou     	Xiong	    Software Engineer	2016-10-05
-Michaela	Michaelson	District Manager	2015-12-19
-Jake	    Jacobson	Programmer
-Jacquelyn	Jackson	    DBA
-Sally	    Webber	    Web Developer   	2015-12-18
-
-
-Example Output
-Name                | Position          | Separation Date
---------------------|-------------------|----------------
-Jacquelyn Jackson   | DBA               |
-Jake Jacobson       | Programmer        |
-John Johnson        | Manager           | 2016-12-31
-Michaela Michaelson | District Manager  | 2015-12-19
-Sally Weber         | Web Developer     | 2015-12-18
-Tou Xiong           | Software Engineer | 2016-10-05
-Constraint
-Store the data using a list of maps.
- */
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
